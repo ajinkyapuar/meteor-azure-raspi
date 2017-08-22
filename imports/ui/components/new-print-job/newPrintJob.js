715fed81141b1ddf83db388ab0aa33bee044d044
Ajinkya <albus702@gmail.com>
2017-08-22 22:29:14 +0530

    new job html & cb

--- imports/ui/components/new-print-job/newPrintJob.js
+++ imports/ui/components/new-print-job/newPrintJob.js
@@ -1,1 +1,10 @@
+import { Template } from 'meteor/templating';
+
 import './newPrintJob.html';
+
+Template.newPrintJob.events({
+	'submit #newJob'(event, instance){
+		event.preventDefault();
+		console.log("New Job");
+	}
+});
